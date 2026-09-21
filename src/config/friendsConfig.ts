e06aa4a84a72463ee90d725c6dc44c4c97e43398
whxhhqsw <323988166+whxhhqsw@users.noreply.github.com>
2026-09-22 00:13:15 +0800

    feat: add friend link 3a0.top

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -23,6 +23,15 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
+		title: "Kwansh_RL's blog",
+		imgurl: "https://3a0.top/logo.png",
+		desc: "Cyberspace Security, CTF, Web Security, and AI Security.",
+		siteurl: "https://3a0.top/",
+		tags: ["Blog", "Security"],
+		weight: 10,
+		enabled: true,
+	},
+	{
 		title: "夏夜流萤",
 		imgurl:
 			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
